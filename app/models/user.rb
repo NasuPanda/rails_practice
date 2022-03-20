8668df191b13699586b3e153676caf818b06cb20
Westen0511 <xiyuanjunzhi@gmail.com>
2022-03-21 07:35:07 +0900

    feat: Add user model to remember_token and remember

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save { email.downcase! }
 
   VALID_EMAIL_REGEX =  /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -11,11 +12,24 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, length: { minimum: 6 }
 
-  # 渡された文字列のハッシュ値を返す
-  def User.digest(string)
-    # コストパラメータをテスト環境では最小に、本番環境ではセキュアにする
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine::cost
-    BCrypt::Password.create(string, cost: cost)
+  # ユーザーを記憶する
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  class << self
+    # 渡された文字列のハッシュ値を返す
+    def User.digest(string)
+      # コストパラメータをテスト環境では最小に、本番環境ではセキュアにする
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine::cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+    # ランダムなトークンを返す
+    def User.new_token
+      SecureRandom.urlsafe_base64
+    end
   end
 end
