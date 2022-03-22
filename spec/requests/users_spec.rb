8c416326b46283fb0e85ae0835e2637454d51ad6
Westen0511 <xiyuanjunzhi@gmail.com>
2022-03-22 21:28:25 +0900

    feat: Add before filter and test

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -20,7 +20,6 @@ RSpec.describe "Users", type: :request do
 
     context "with valid information" do
       it "creates a user" do
-        # get signup_path
         expect{
           post users_path, params: { user: valid_user_params }
         }.to change(User, :count).by 1
@@ -32,7 +31,7 @@ RSpec.describe "Users", type: :request do
         expect(response).to redirect_to created_user
       end
 
-      it "is logged in" do
+      it "is logged in when successfully created" do
         post users_path, params: { user: valid_user_params }
         expect(is_logged_in?).to be_truthy
       end
@@ -40,7 +39,6 @@ RSpec.describe "Users", type: :request do
 
     context "with invalid information" do
       it "doesn't create a user" do
-        get signup_path
         expect{
           post users_path, params: { user: invalid_user_params }
         }.to_not change(User, :count)
@@ -48,56 +46,115 @@ RSpec.describe "Users", type: :request do
     end
   end
 
+  describe "#edit" do
+    let(:user) { FactoryBot.create(:user) }
+
+    context "as a logged in user" do
+      before do
+        log_in_request(user)
+      end
+
+      it "has a correct title" do
+        get edit_user_path(user)
+        expect(response.body).to include full_title("Edit user")
+      end
+    end
+
+    context "as a non logged in user" do
+      it "has a flash" do
+        get edit_user_path(user)
+        expect(flash).to be_any
+      end
+
+      it "redirects to login_path" do
+        get edit_user_path(user)
+        expect(response).to redirect_to login_path
+      end
+    end
+
+  end
+
   describe "#update" do
     let(:user) { FactoryBot.create(:user) }
 
-    context "with valid information" do
+    context "as a logged in user" do
       before do
-        @another_user_params = { name: "Another name",
-          email: "another@gmail.com",
-          password: "",
-          password_confirmation: "" }
+        log_in_request(user)
       end
 
-      it "edits a user" do
-        patch user_path(user), params: { user: @another_user_params }
-        user.reload
-        expect(user.name).to eq @another_user_params[:name]
-        expect(user.email).to eq @another_user_params[:email]
+      context "with valid information" do
+        before do
+          @another_user_params = { name: "Another name",
+            email: "another@gmail.com",
+            password: "",
+            password_confirmation: "" }
+          patch user_path(user), params: { user: @another_user_params }
+        end
+
+        it "edits a user" do
+          user.reload
+          expect(user.name).to eq @another_user_params[:name]
+          expect(user.email).to eq @another_user_params[:email]
+        end
+
+        it "redirects to users/id" do
+          expect(response).to redirect_to user
+        end
+
+        it "has a success flash" do
+          expect(flash).to be_any
+        end
       end
 
-      it "redirects to users/id" do
-        patch user_path(user), params: { user: @another_user_params }
-        expect(response).to redirect_to user
-      end
+      context "with invalid information" do
+        before do
+          @invalid_user_params = FactoryBot.attributes_for(:invalid_user)
+          patch user_path(user), params: { user: @invalid_user_params }
+        end
 
-      it "has a flash" do
-        patch user_path(user), params: { user: @another_user_params }
-        expect(flash).to be_any
+        it "doesn't edit a user" do
+          user.reload
+          expect(user.name).to_not eq @invalid_user_params[:name]
+          expect(user.email).to_not eq @invalid_user_params[:email]
+          expect(user.password).to_not eq @invalid_user_params[:password]
+          expect(user.password_confirmation).to_not eq @invalid_user_params[:password_confirmation]
+        end
+
+        it "redirects to edit page" do
+          expect(response.body).to include full_title("Edit user")
+        end
+
+        it "has correct error messages of 'The form contains 3 errors'" do
+          expect(response.body).to include "The form contains 3 errors"
+        end
       end
     end
 
-    context "with invalid information" do
-      let(:invalid_user_params) { FactoryBot.attributes_for(:invalid_user) }
-      it "doesn't edit a user" do
-        patch user_path(user), params: { user: invalid_user_params }
-        user.reload
-        expect(user.name).to_not eq invalid_user_params[:name]
-        expect(user.email).to_not eq invalid_user_params[:email]
-        expect(user.password).to_not eq invalid_user_params[:password]
-        expect(user.password_confirmation).to_not eq invalid_user_params[:password_confirmation]
-      end
+    context "as a non logged in user" do
+        before do
+          @another_user_params = { name: "Another name",
+            email: "another@gmail.com",
+            password: "",
+            password_confirmation: "" }
 
-      it "redirects to edit page" do
-        patch user_path(user), params: { user: invalid_user_params }
-        expect(response.body).to include full_title("Edit user")
-      end
+          patch user_path(user), params: { user: @another_user_params }
+        end
 
-      it "has correct error messages of 'The form contains 3 errors'" do
-        patch user_path(user), params: { user: invalid_user_params }
-        expect(response.body).to include "The form contains 3 errors"
-      end
+        it "can't edit" do
+          user.reload
+          expect(user.name).to_not eq @another_user_params[:name]
+          expect(user.email).to_not eq @another_user_params[:email]
+        end
+
+        it "redirects to root" do
+          expect(response).to redirect_to login_path
+        end
+
+        it "has a error flash" do
+          expect(flash).to be_any
+        end
     end
   end
+
 
 end
--- spec/support/login.rb
+++ spec/support/login.rb
@@ -1,5 +1,18 @@
 module LoginSupport
 
+  def log_in_system(user)
+    visit login_path
+
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Log in'
+  end
+
+  def log_in_request(user)
+    post login_path, params: { session: { email: user.email,
+      password: user.password } }
+  end
+
   def is_logged_in?
     !session[:user_id].nil?
   end
