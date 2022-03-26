0e0aa9d6bb1f1151aa16d9e925124790113e7302
Westen0511 <xiyuanjunzhi@gmail.com>
2022-03-26 09:44:58 +0900

    test: Add validation tests

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Micropost, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryBot.create(:user) }
+  let(:micropost) { Micropost.new(content: "Test post", user_id: user.id) }
+
+  context "with valid attributes" do
+    it "is valid" do
+      expect(micropost).to be_valid
+    end
+
+    it "is valid with content equal to the boundary value" do
+      micropost.content = "a" * 140
+      expect(micropost).to be_valid
+    end
+  end
+
+  context "with invalid attributes" do
+    it "is invalid without user_id" do
+      micropost.user_id = nil
+      expect(micropost).to_not be_valid
+    end
+
+    it "is invalid without content" do
+      micropost.content = ""
+      expect(micropost).to_not be_valid
+    end
+
+    it "is invalid with a too long content" do
+      micropost.content = "a" * 141
+      expect(micropost).to_not be_valid
+    end
+  end
 end
