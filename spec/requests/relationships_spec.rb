e4199c894064a3b40d909f51cad708adc015426a
Westen0511 <xiyuanjunzhi@gmail.com>
2022-03-27 19:36:36 +0900

    test: Add follow/unfollow function by standard-way/Ajax

--- spec/requests/relationships_spec.rb
+++ spec/requests/relationships_spec.rb
@@ -3,9 +3,25 @@ require 'rails_helper'
 RSpec.describe "Relationships", type: :request do
   describe "#create" do
     context "as a logged in user" do
-      # it "" do
-      # end
+      before do
+        @user = FactoryBot.create(:user)
+        @other_user = FactoryBot.create(:user)
+        log_in @user
+      end
+
+      it "creates a relationship by the standard way" do
+        expect{
+          post relationships_path, params: { followed_id: @other_user.id }
+        }.to change(Relationship, :count).by 1
+      end
+
+      it "creates a relationship by the Ajax" do
+        expect{
+          post relationships_path, params: { followed_id: @other_user.id }, xhr: true
+        }.to change(Relationship, :count).by 1
+      end
     end
+
     context "as a non-logged in user" do
       it "redirects to login_path" do
         post relationships_path
@@ -21,12 +37,33 @@ RSpec.describe "Relationships", type: :request do
   end
 
   describe "#destroy" do
-    let!(:relationship) { FactoryBot.create(:relationship) }
     context "as a logged in user" do
-      # it "" do
-      # end
+      before do
+        @user = FactoryBot.create(:user)
+        @other_user = FactoryBot.create(:user)
+        log_in @user
+      end
+
+      it "deletes a relationship by the standard way" do
+        @user.follow(@other_user)
+        created_relationship = @user.active_relationships.find_by(followed_id: @other_user.id)
+        expect{
+          delete relationship_path(created_relationship)
+        }.to change(Relationship, :count).by -1
+      end
+
+      it "deletes a relationship by the Ajax" do
+        @user.follow(@other_user)
+        created_relationship = @user.active_relationships.find_by(followed_id: @other_user.id)
+        expect{
+          delete relationship_path(created_relationship), xhr: true
+        }.to change(Relationship, :count).by -1
+      end
     end
+
     context "as a non-logged in user" do
+      let!(:relationship) { FactoryBot.create(:relationship) }
+
       it "redirects to login_path" do
         delete relationship_path(relationship)
         expect(response).to redirect_to login_path
