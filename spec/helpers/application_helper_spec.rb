a4b73c5eecebf396df61f5ba733e08e3349565ac
Westen0511 <xiyuanjunzhi@gmail.com>
2022-03-30 08:20:44 +0900

    style: Modified helper-specs to RuboCop-based stylef

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,20 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
-  describe "#full_title" do
-    let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+  describe '#full_title' do
+    let(:base_title) { 'Ruby on Rails Tutorial Sample App' }
 
-    context "exist argment string" do
-      it "returns argment string + base title" do
-        expect(full_title("Page Title")).to eq "Page Title | #{base_title}"
+    context 'with an argment string' do
+      it 'returns argment string + base title' do
+        expect(full_title('Page Title')).to eq "Page Title | #{base_title}"
       end
     end
 
-    context "not exist argment string" do
-      it "returns base title" do
-        expect(full_title).to eq "#{base_title}"
+    context 'without an argment string' do
+      it 'returns base title' do
+        expect(full_title).to eq base_title
       end
     end
   end
-
 end
