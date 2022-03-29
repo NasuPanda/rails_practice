900ca2af17754d397bb256ca3b2c8eb4e590df0a
Westen0511 <xiyuanjunzhi@gmail.com>
2022-03-30 06:28:36 +0900

    style: Modified to RuboCop-based style

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -51,10 +51,10 @@ class PasswordResetsController < ApplicationController
     end
 
     def check_expiretion
-      if @user.password_reset_expired?
-        flash[:danger] = 'Password reset has expired'
-        redirect_to new_password_reset_url
-      end
+      return unless @user.password_reset_expired?
+
+      flash[:danger] = 'Password reset has expired'
+      redirect_to new_password_reset_url
     end
 
     def user_params
