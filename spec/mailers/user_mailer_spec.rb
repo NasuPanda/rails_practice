3c5b457b2bd5c1bdc5ea8e7785150a95193a7267
Westen0511 <xiyuanjunzhi@gmail.com>
2022-03-30 22:24:09 +0900

    style: Modified spec/mailers to RuboCop-based style

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,27 +1,27 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe UserMailer, type: :mailer do
   let(:user) { FactoryBot.create(:user) }
-  let(:from_address) { "noreply@example.com" }
+  let(:from_address) { 'noreply@example.com' }
 
-  describe "account_activation" do
-    let(:mail) { UserMailer.account_activation(user) }
+  describe 'account_activation' do
+    let(:mail) { described_class.account_activation(user) }
 
-    describe "header" do
+    describe 'header' do
       it "sends to the correct user's email address" do
         expect(mail.to).to eq [user.email]
       end
 
-      it "sends with a correct subject" do
-        expect(mail.subject).to eq("Account activation")
+      it 'sends with a correct subject' do
+        expect(mail.subject).to eq('Account activation')
       end
 
-      it "sends from a correct email address" do
+      it 'sends from a correct email address' do
         expect(mail.from).to eq [from_address]
       end
     end
 
-    describe "body" do
+    describe 'body' do
       it "includes a user's name" do
         expect(mail.body.encoded).to match(user.name)
       end
@@ -30,41 +30,41 @@ RSpec.describe UserMailer, type: :mailer do
         expect(mail.body.encoded).to match(CGI.escape(user.email))
       end
 
-      it "includes the activation token" do
+      it 'includes the activation token' do
         expect(mail.body.encoded).to match(user.activation_token)
       end
     end
   end
 
-  describe "password_reset" do
-    let(:mail) { UserMailer.password_reset(user) }
+  describe 'password_reset' do
+    let(:mail) { described_class.password_reset(user) }
+
     before do
       user.reset_token = User.new_token
     end
 
-    describe "header" do
+    describe 'header' do
       it "sends to the user's email address" do
         expect(mail.to).to eq [user.email]
       end
 
-      it "sends with the correct subject" do
-        expect(mail.subject).to eq("Password reset")
+      it 'sends with the correct subject' do
+        expect(mail.subject).to eq('Password reset')
       end
 
-      it "sends from the correct email address" do
+      it 'sends from the correct email address' do
         expect(mail.from).to eq [from_address]
       end
     end
 
-    describe "body" do
+    describe 'body' do
       it "includes the user's email" do
         expect(mail.body.encoded).to match(CGI.escape(user.email))
       end
 
-      it "includes the reses token" do
+      it 'includes the reses token' do
         expect(mail.body.encoded).to match(user.reset_token)
       end
     end
   end
-
 end
