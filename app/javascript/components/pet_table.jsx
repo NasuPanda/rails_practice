7ab151f209e6942dfac9ace318819bf313e6d311
Westen0511 <xiyuanjunzhi@gmail.com>
2022-04-01 21:15:47 +0900

    rename practice18-1 jsx-file

---
+++ app/javascript/components/pet_table.jsx
@@ -0,0 +1,94 @@
+import { useState } from 'react';
+
+const PetsTable = (props) => {
+  const pets = props.pets;
+
+  const createTable = (elements) => {
+    const table = elements.map((element) =>
+      <tr>
+        <td>{element.name}</td>
+        <td>{element.species}</td>
+      </tr>
+    );
+    return table;
+  }
+  const [table, setTable] = useState(createTable(pets));
+
+  const handleClickAllAnimalVisibilityToggleButton = () => {
+    const displayAnimals = createTable(pets);
+    setTable(displayAnimals);
+  }
+
+  const handleClickCatVisibilityToggleButton = () => {
+    const cats = pets.filter(pet => pet.species !== "dog");
+    const displayCats = createTable(cats);
+    setTable(displayCats);
+  }
+
+  const handleClickDogVisibilityToggleButton = () => {
+    const dogs = pets.filter(pet => pet.species !== "cat");
+    const displayDogs = createTable(dogs);
+    setTable(displayDogs);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const species = e.target[0].value;
+    const name = e.target[1].value;
+
+    if (!species || !name) {
+      alert("Please input")
+      return
+    } else {
+      pets.push({ name: name, species: species })
+      setTable(createTable(pets))
+    }
+  }
+
+  return (
+    <>
+    {table}
+    <button onClick={handleClickAllAnimalVisibilityToggleButton}>
+      All animal
+    </button>
+    <button onClick={handleClickCatVisibilityToggleButton}>
+      Cat
+    </button>
+    <button onClick={handleClickDogVisibilityToggleButton}>
+      Dog
+    </button>
+    {/* フォーム */}
+    <form onSubmit={handleSubmit}>
+      <label>
+        Select Dog/Cat
+        <select>
+          <option value="dog">dog</option>
+          <option value="cat">cat</option>
+        </select>
+      </label>
+      <label>
+        Put a name
+        <input name="name" type="text" />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+    </>
+  );
+}
+
+const App = () => {
+  const pets = [
+    { name: "Pochi", species: "dog" },
+    { name: "Tama", species: "cat" },
+    { name: "Mike", species: "cat" },
+    { name: "Hachi", species: "dog" },
+  ];
+  return(
+    <PetsTable
+      pets={pets}>
+    </PetsTable>
+    )
+};
+
+export default App;
